e5ae2d94943776fe1270e0355b61ebaf56d1038f
opee2012 <47112091+opee2012@users.noreply.github.com>
2021-09-20 21:08:55 -0400

    added comments to main.cc

--- main.cc
+++ main.cc
@@ -1,3 +1,15 @@
+// Write a program that queries the user for file name, then reads that file name and performs the following operations:
+//
+//    - Count the total number of lines in the file
+//    - Count the total number of words in the file
+//    - Count the total number of characters in the file
+//
+// The program should output these numbers (in that order) on a single line, as well as the file name, separated by tabs.
+//
+// Jacob Borth
+// 20-Sep-21
+// CSCI-208
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -5,6 +17,7 @@
 
 using namespace std;
 
+// Function Prototypes
 string FileCheck();
 void CountLines(string);
 void CountWords(string);
@@ -20,28 +33,30 @@ int main()
 {
     string lineBuffer;
     
-    FileCheck();
+    FileCheck();                            // String function for input file handling
     
-    getline(inputFile, lineBuffer, '\0');
+    getline(inputFile, lineBuffer, '\0');   // using getline to convert input file into a string
 
-    CountLines(lineBuffer);
-    CountWords(lineBuffer);
-    CountCharacters(lineBuffer);
+    CountLines(lineBuffer);                 // Void function to count the total number of lines in the input file.
+    CountWords(lineBuffer);                 // Void function to count the total number of words in the input file.
+    CountCharacters(lineBuffer);            // Void function to count the total number of characters in the input file.
     
     cout << lines << "\t" << words << "\t" << characters << "\t" << inputFileName;
     
     return 0;
 }
 
+// String function for input file handling
 string FileCheck()
 {
     do
     {
+        // Get the input file name from the user
         cout << "Please enter a valid file name: ";
         cin >> inputFileName;
         
+        // Open the input file and check if it opens. If it does not open, ask again.
         inputFile.open(inputFileName);
-        
         if (!inputFile.is_open())
         {
             cout << "Could not open file " << inputFileName << endl;
@@ -52,11 +67,12 @@ string FileCheck()
     return (inputFileName);
 }
 
+// Void function to count the total number of lines in the input file.
 void CountLines(string readLines)
 {
-    stringstream l(readLines);
-    string lineHolder;
-    bool lineCheck;
+    stringstream l(readLines);  // stringstring 'l' created with the string passed into function
+    string lineHolder;          // more or less a buffer for strings created to represent lines of the input file
+    bool lineCheck;             // boolean for checking when do-while reaches end of stringstream
     
     do
     {
@@ -64,24 +80,28 @@ void CountLines(string readLines)
         lines++;
         if (l.eof())
         {
-            lineCheck = true;
+            lineCheck = true;   // when end of stringstream is not yet reached
         }
         
     } while (lineCheck == false);
 }
 
+// Void function to count the total number of words in the input file.
 void CountWords(string readWords)
 {
-    stringstream w(readWords);
-    string wordBuffer;
+    stringstream w(readWords);  // stringstring 'w' created with the string passed into function
+    string wordBuffer;          // more or less a buffer for strings created to represent words of the input file
 
+    // while loop inserts all characters from stringstream 'w' into a string stopping at an empty character
+    // as well as ignoring empty characters.
     while (w >> wordBuffer)
     {
         words++;
     }
 }
 
+// Void function to count the total number of characters in the input file.
 void CountCharacters(string readCharacters)
 {
-    characters += readCharacters.length();
+    characters += readCharacters.length();  // reading the length of each string passed into function
 }
